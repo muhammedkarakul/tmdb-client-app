88e114f3a8cfc8e700a6595dd4b95f89ffe8bfcb
Muhammed Karakul <muhammedkarakul@Muhammeds-MacBook-Pro.local>
2021-12-25 13:56:21 +0300

    Project created.

---
+++ TMDB Client App/TMDB Client App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB Client App
+//
+//  Created by Muhammed Karakul on 25.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
